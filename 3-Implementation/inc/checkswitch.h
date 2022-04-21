afa48b1179e036181ed63bdf3ad62b18460cb930
ri7aad <101317269+ri7aad@users.noreply.github.com>
2022-04-22 01:56:16 +0530

    Add files via upload

---
+++ 3-Implementation/inc/checkswitch.h
@@ -0,0 +1,15 @@
+/**
+ * @file checkswitch.h
+ * @author Rishad Rahi
+ */
+#ifndef _CHECKSWITCH_
+#define _CHECKSWITCH_
+
+
+
+#include <avr/io.h>
+
+
+void checkswitch(void);
+
+#endif
---
+++ 3-Implementation/inc/relay.h
@@ -0,0 +1,14 @@
+/**
+ * @file relay.h
+ * @author Rishad Rahi
+ */
+
+#ifndef _RELAY_
+#define _RELAY_
+
+#include <avr/io.h>
+
+
+void relay(void);
+
+#endif
